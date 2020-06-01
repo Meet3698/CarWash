25c2f388b8d163b0b6da32eedc077dc2d8d75a38
Meet <49397344+Meet3698@users.noreply.github.com>
2020-06-02 00:25:01 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://meet:Meet@3698@anonymous-edqd9.mongodb.net/CarWash?retryWrites=true&w=majority',{useNewUrlParser:true},(err)=>{
+mongoose.connect('mongodb+srv://meet:<password>@anonymous-edqd9.mongodb.net/CarWash?retryWrites=true&w=majority',{useNewUrlParser:true},(err)=>{
     if(!err) console.log('connected!');
     else console.log(err);
 })
